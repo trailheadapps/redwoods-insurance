ab3346904e72cdfd582af2e64204cd34c96e8a27
Kevin Poorman <kpoorman@salesforce.com>
2020-07-13 16:55:12 -0400

    first test. maybe.

--- redwoods-insurance-salesforce/force-app/main/default/lwc/incidentImageCarousel/__tests__/incidentImageCarousel.test.js
+++ redwoods-insurance-salesforce/force-app/main/default/lwc/incidentImageCarousel/__tests__/incidentImageCarousel.test.js
@@ -1,5 +1,18 @@
 import { createElement } from 'lwc';
 import IncidentImageCarousel from 'c/incidentImageCarousel';
+import { registerApexTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
+import { getRecord } from 'lightning/uiRecordApi';
+import { getRelatedPictures } from '@salesforce/apex/IncidentController.findRelatedFiles';
+import { registerLdsTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
+
+// Realistic data with two images.
+const mockTwoImages = require('./data/twoImages.json');
+// Register an Apex Wire adapter.
+const getRelatedPicturesAdapter = registerApexTestWireAdapter(
+    getRelatedPictures
+);
+
+const getRecordAdapter = registerLdsTestWireAdapter(getRecord);
 
 describe('c-incident-image-carousel', () => {
     afterEach(() => {
@@ -18,5 +31,25 @@ describe('c-incident-image-carousel', () => {
 
         const labelElement = element.shadowRoot.querySelector('p');
         expect(labelElement.textContent).toContain('no images');
+    });
+
+    it('renders a picture when one is present', () => {
+        // Emit data from @wire
+        getRecordAdapter.emit('mockRecordId');
+        getRelatedPicturesAdapter.emit(mockTwoImages);
+
+        // Create initial element
+        const element = createElement('c-incident-image-carousel', {
+            is: IncidentImageCarousel
+        });
+        document.body.appendChild(element);
+
+        return Promise.resolve().then(() => {
+            const imageEls = element.shadowRoot.querySelectorAll(
+                'lightning-carousel-image'
+            );
+            console.log(JSON.stringify(imageEls));
+            expect(imageEls).not.toBe(null);
+        });
     });
 });
