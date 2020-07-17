2932587b7096fc5652033752da0cb251b15ee788
Kevin Poorman <codefriar@users.noreply.github.com>
2020-07-17 13:48:06 -0400

    Prerelease/summer20 lwc tests for incidentImageCarousel (#108)
    Adds LWC testing for the IncidentImageCarousel component

--- redwoods-insurance-salesforce/force-app/main/default/lwc/incidentImageCarousel/__tests__/incidentImageCarousel.test.js
+++ redwoods-insurance-salesforce/force-app/main/default/lwc/incidentImageCarousel/__tests__/incidentImageCarousel.test.js
@@ -1,5 +1,15 @@
 import { createElement } from 'lwc';
 import IncidentImageCarousel from 'c/incidentImageCarousel';
+import { registerApexTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
+import { getRelatedPictures } from '@salesforce/apex/IncidentController.findRelatedFiles';
+
+// Realistic data with two images.
+const mockTwoImages = require('./data/twoImages.json');
+// Register an Apex Wire adapter.
+const getRelatedPicturesAdapter = registerApexTestWireAdapter(
+    getRelatedPictures
+);
+const mockRecordId = '5001700000pJRRTAA4';
 
 describe('c-incident-image-carousel', () => {
     afterEach(() => {
@@ -7,6 +17,22 @@ describe('c-incident-image-carousel', () => {
         while (document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
+    });
+
+    it('invokes the wire adapter with default properties', () => {
+        // Create initial element
+        const element = createElement('c-incident-image-carousel', {
+            is: IncidentImageCarousel
+        });
+        element.recordId = mockRecordId;
+        document.body.appendChild(element);
+
+        return Promise.resolve().then(() => {
+            expect(getRelatedPicturesAdapter.getLastConfig()).toEqual({
+                caseId: mockRecordId,
+                fileType: 'IMAGE'
+            });
+        });
     });
 
     it('renders no pictures by default', () => {
@@ -17,6 +43,50 @@ describe('c-incident-image-carousel', () => {
         document.body.appendChild(element);
 
         const labelElement = element.shadowRoot.querySelector('p');
-        expect(labelElement.textContent).toContain('no images');
+        expect(labelElement.textContent).toContain(
+            'There are currently no images of damage for this case.'
+        );
+    });
+
+    it('renders two pictures when the wire mock returns two', () => {
+        // Create initial element
+        const element = createElement('c-incident-image-carousel', {
+            is: IncidentImageCarousel
+        });
+        document.body.appendChild(element);
+
+        // Emit data from @wire
+        getRelatedPicturesAdapter.emit(mockTwoImages);
+
+        return Promise.resolve().then(() => {
+            const imageEls = element.shadowRoot.querySelectorAll(
+                'lightning-carousel-image'
+            );
+            expect(imageEls.length).toEqual(mockTwoImages.length);
+            imageEls.forEach(function (value, index) {
+                expect.stringMatching(
+                    imageEls[index].src,
+                    /mockTwoImages[index].Id$/
+                );
+            });
+        });
+    });
+
+    it('renders no pictures when the wire adapter returns something other than an aray', () => {
+        // Create initial element
+        const element = createElement('c-incident-image-carousel', {
+            is: IncidentImageCarousel
+        });
+        document.body.appendChild(element);
+
+        // forcibly emit an object, instead of an array.
+        getRelatedPicturesAdapter.emit({});
+
+        return Promise.resolve().then(() => {
+            const labelElement = element.shadowRoot.querySelector('p');
+            expect(labelElement.textContent).toContain(
+                'There are currently no images of damage for this case.'
+            );
+        });
     });
 });
--- redwoods-insurance-salesforce/force-app/main/default/lwc/incidentImageCarousel/incidentImageCarousel.js
+++ redwoods-insurance-salesforce/force-app/main/default/lwc/incidentImageCarousel/incidentImageCarousel.js
@@ -1,23 +1,15 @@
 import { LightningElement, wire, api } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
 import getRelatedPictures from '@salesforce/apex/IncidentController.findRelatedFiles';
 
 export default class IncidentImageCarousel extends LightningElement {
     @api recordId;
     urls;
-    pictures;
-
-    @wire(getRecord, {
-        recordId: '$recordId'
-    })
-    kase;
 
     @wire(getRelatedPictures, {
         caseId: '$recordId',
         fileType: 'IMAGE'
     })
     wiredPictures(pictures) {
-        this.pictures = pictures;
         if (pictures.data) {
             const files = pictures.data;
             if (Array.isArray(files) && files.length) {
